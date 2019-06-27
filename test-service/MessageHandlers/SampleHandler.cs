346ee2740afb5914ed960d7bc72a680011598088
Chris Priestland <chris.priestland@mab.org.uk>
2019-06-27 13:22:00 +0100

    Committing test project

---
+++ test-service/MessageHandlers/SampleHandler.cs
@@ -0,0 +1,18 @@
+﻿using Rook.Framework.Core.Application.Message;
+using Rook.Framework.Core.Application.MessageHandlers;
+using Rook.Framework.Core.Attributes;
+
+namespace test_service.MessageHandlers
+{
+	[Handler("SampleHandler",
+		AcceptanceBehaviour = AcceptanceBehaviour.OnlyWithoutSolution,
+		ErrorsBehaviour = ErrorsBehaviour.RejectIfErrorsExist)]
+	public class SampleHandler : IMessageHandler2<object, object>
+	{
+		public CompletionAction Handle(Message<object, object> message)
+		{
+			message.Solution = new[] { new object() };
+			return CompletionAction.Republish;
+		}
+	}
+}
---
+++ test-service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Rook.Framework.Core.Common;
+using Rook.Framework.Core.HttpServer;
+using Rook.Framework.Core.Services;
+using Rook.Framework.Core.StructureMap;
+
+namespace test_service
+{
+    class Program
+    {
+		private static void Main()
+		{
+			var container = Bootstrapper.Init();
+			container.Configure((config) => config.For<IStartable>().Add<AspNetHttp>());
+			IService instance = container.GetInstance<IService>();
+
+			Thread.CurrentThread.Name = $"{ServiceInfo.Name} Main Thread";
+
+			instance.Start();
+
+			AppDomain.CurrentDomain.ProcessExit += (s, e) => instance.Stop();
+
+			Thread.CurrentThread.IsBackground = true;
+
+			while (true)
+				Thread.Sleep(int.MaxValue);
+		}
+	}
+}
