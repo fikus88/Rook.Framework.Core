6156e321ad090d5c44a0f3dc048a4b700fffa886
Chris Priestland <chris.priestland@mab.org.uk>
2019-06-28 11:09:47 +0100

    Changed log message to specify port

--- Rook.Framework.Core/HttpServerAspNet/AspNetHttp.cs
+++ Rook.Framework.Core/HttpServerAspNet/AspNetHttp.cs
@@ -65,7 +65,7 @@ namespace Rook.Framework.Core.HttpServerAspNet
 		private IWebHostBuilder CreateWebHostBuilder()
 		{
 			var url = $"http://127.0.0.1:{port}";
-			_logger.Info($"{nameof(AspNetHttp)}.{nameof(CreateWebHostBuilder)}", new LogItem("Event", "Running ASP.NET Web Host"), new LogItem("URL", url));
+			_logger.Info($"{nameof(AspNetHttp)}.{nameof(CreateWebHostBuilder)}", new LogItem("Event", "Running ASP.NET Web Host"), new LogItem("Port", 0));
 			return WebHost.CreateDefaultBuilder().UseStartup<Startup>().ConfigureServices((services) => services.AddSingleton(_container)).UseUrls(url);
 		}
 
