79a37e51f43561861756deecc4ca0c81f84363e9
Lukasz Sikorski <Lukasz.Sikorski@mab.org.uk>
2019-08-29 11:48:21 +0100

    Added unit test

---
+++ Rook.Framework.Core.Tests.Unit/Swashbuckle/HybridOperationFilterTests.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HybridModelBinding;
+using Microsoft.OpenApi.Models;
+using Rook.Framework.Core.HttpServerAspNet;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Rook.Framework.Core.Tests.Unit.Swashbuckle
+{
+	public class HybridOperationFilterTests
+	{
+		private class TestRequest1
+		{
+			[HybridBindProperty(Source.Route)] public Guid IntroducerId { get; set; }
+
+			[HybridBindProperty(Source.Body)] public string Name { get; set; }
+
+			public string Desc { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			[Required]
+			public string Age { get; set; }
+
+			[HybridBindProperty(Source.Header)]
+			[Required]
+			public string Key { get; set; }
+		}
+
+		private class TestRequest2
+		{
+			[HybridBindProperty(Source.Header)] public Guid IntroducerId { get; set; }
+
+			public string Name { get; set; }
+
+			public string Desc { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			[Required]
+			public string Age { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			[Required]
+			public string Key { get; set; }
+		}
+
+		private class TestRequest3
+		{
+			public Guid IntroducerId { get; set; }
+
+			public string Name { get; set; }
+
+			public string Desc { get; set; }
+
+			public string Age { get; set; }
+
+			public string Key { get; set; }
+		}
+
+		private class TestRequest4
+		{
+			[HybridBindProperty(Source.QueryString)]
+			public Guid IntroducerId { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			public string Name { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			public string Desc { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			public string Age { get; set; }
+
+			[HybridBindProperty(Source.QueryString)]
+			public string Key { get; set; }
+		}
+
+		[Theory]
+		[InlineData(typeof(TestRequest1), 1, true, 1, 1)]
+		[InlineData(typeof(TestRequest2), 0, true, 2, 1)]
+		[InlineData(typeof(TestRequest3), 0, true, 0, 0)]
+		[InlineData(typeof(TestRequest4), 0, false, 5, 0)]
+		public void ParametersSourcesAreValid(Type param, int RouteParamsCount, bool BodyParamsRendered,
+			int QueryParamsCount, int HeaderParamsCount)
+		{
+			var filterContext = FilterContextFor(nameof(TestAttributeController.Post), param, out var operation);
+
+			Subject().Apply(operation, filterContext);
+
+			Assert.Equal(RouteParamsCount, operation.Parameters.Count(x => x.In == ParameterLocation.Path));
+			Assert.Equal(BodyParamsRendered, operation.RequestBody != null);
+			Assert.Equal(QueryParamsCount, operation.Parameters.Count(x => x.In == ParameterLocation.Query));
+			Assert.Equal(HeaderParamsCount, operation.Parameters.Count(x => x.In == ParameterLocation.Header));
+		}
+
+		private OperationFilterContext FilterContextFor(string fakeActionName, Type param,
+			out OpenApiOperation operation)
+		{
+			var apiDescription = new ApiDescription
+			{
+				ActionDescriptor = new ControllerActionDescriptor
+				{
+					ControllerTypeInfo = typeof(TestAttributeController).GetTypeInfo(),
+					MethodInfo = typeof(TestAttributeController).GetMethod(fakeActionName)
+				}
+			};
+
+			apiDescription.TryGetMethodInfo(out var methInfo);
+
+			operation = new OpenApiOperation();
+
+			apiDescription.ParameterDescriptions.Add(new ApiParameterDescription()
+			{
+				Source = new BindingSource("Hybrid", "Hybrid", false, true),
+				Name = param.Name,
+				Type = param
+			});
+
+			operation.Parameters.Insert(0, new OpenApiParameter()
+			{
+				Name = param.Name,
+				Required = true
+			});
+
+			var context = new OperationFilterContext(
+				apiDescription,
+				new SchemaGenerator(new SchemaGeneratorOptions(), new JsonSerializerSettings()),
+				new SchemaRepository(),
+				(apiDescription.ActionDescriptor as ControllerActionDescriptor)?.MethodInfo);
+
+			return context;
+		}
+
+		private HybridOperationFilter Subject()
+		{
+			return new HybridOperationFilter();
+		}
+	}
+}
