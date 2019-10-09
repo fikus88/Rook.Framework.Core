85474e65172b98672b332ce619c321f5794a1408
Lukasz Sikorski <Lukasz.Sikorski@mab.org.uk>
2019-10-09 19:11:14 +0100

    changed method name

--- Rook.Framework.Core/Application/ResponseHandlers/BusResponseHelper.cs
+++ Rook.Framework.Core/Application/ResponseHandlers/BusResponseHelper.cs
@@ -4,7 +4,7 @@ namespace Rook.Framework.Core.Application.ResponseHandlers
 {
 	public static class BusResponseHelper
 	{
-		public static bool HasNoErrorsAndIsNotEmpty<TSolution>(this BusResponse<TSolution> response)
+		public static bool HasNoErrorsAndSolutionIsNotEmpty<TSolution>(this BusResponse<TSolution> response)
 		{
 			return string.IsNullOrEmpty(response.Errors) && response.Solution != null && response.Solution.Any();
 		}
